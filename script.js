46c9c1648c92c01d3fd71df3663fc4d8a8ad354c
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-22 17:40:57 +0800

    Add eventListener to buttons

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
+    let playerSelection = this.textContent;
+    let computerSelection = getComputerChoice();
     let playerValue = CHOICES.indexOf(playerSelection);
     let computerValue = CHOICES.indexOf(computerSelection);
     result = playerValue - computerValue;
@@ -19,36 +21,42 @@ function playRound(playerSelection, computerSelection) {
     return null;
 }
 
-function game() {
-    let score = [0, 0]; // [0] holds the player's score, [1] holds the computer's score
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt("Your choice: ");
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
-        if (!validate(playerSelection)) {
-            console.log("You can only choose 'Rock' / 'Paper' 'Scissors'.")
-            i--;
-            continue;
-        }
-        let winner = playRound(playerSelection, getComputerChoice());
-        if (winner !== null) {
-            score[winner]++;
-        }
-    }
-    outputScore(score);
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
 
-function outputScore(score) {
-    if (score[0] == score[1]) {
-        console.log(`The final score is a draw. ${score[0]} : ${score[1]}.`);
-    } else if (score[0] < score[1]) {
-        console.log(`Sorry, mate, you lost ${score[0]} : ${score[1]}.`);
-    } else {
-        console.log(`Congrats! You won ${score[0]} : ${score[1]}.`);
-    }
-}
+    
+
+
+// function game() {
+//     let score = [0, 0]; // [0] holds the player's score, [1] holds the computer's score
+//     for (let i = 1; i <= 5; i++) {
+//         let playerSelection = prompt("Your choice: ");
+//         playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+//         if (!validate(playerSelection)) {
+//             console.log("You can only choose 'Rock' / 'Paper' 'Scissors'.")
+//             i--;
+//             continue;
+//         }
+//         let winner = playRound(playerSelection, getComputerChoice());
+//         if (winner !== null) {
+//             score[winner]++;
+//         }
+//     }
+//     outputScore(score);
+// }
+
+// function outputScore(score) {
+//     if (score[0] == score[1]) {
+//         console.log(`The final score is a draw. ${score[0]} : ${score[1]}.`);
+//     } else if (score[0] < score[1]) {
+//         console.log(`Sorry, mate, you lost ${score[0]} : ${score[1]}.`);
+//     } else {
+//         console.log(`Congrats! You won ${score[0]} : ${score[1]}.`);
+//     }
+// }
 
 function validate(playerSelection) {
     return CHOICES.includes(playerSelection);
 }
 
-game();
+// game();
